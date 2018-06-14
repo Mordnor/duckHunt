e19a6a612d775fed658d849913e1aa6333ce36e5
qrames <ramque@orange.fr>
2018-06-14 15:57:19 +0200

    the shoot system, the score add the ducks new images on click

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 // Main variables
 var mainElmt = document.getElementById("content");
 var imgDuck = document.createElement("img");
-imgDuck.src = "patrrick-decoupe.png"
+var scoreText = document.getElementById("score");
+document.body.style.backgroundImage = "url(patrrick-font.jpeg)"
+
+ducksImg = ["patrrick-decoupe.png", "johnny_hallyday_002.jpg"];
+imgDuck.src = ducksImg[0];
 // Style
 mainElmt.style.position = "relative";
 mainElmt.style.height = "100vh";
-// divDuck.style.backgroundColor = "red";
 imgDuck.style.width = "150px";
 imgDuck.style.height = "auto";
 imgDuck.style.position = "absolute";
@@ -14,12 +17,24 @@ imgDuck.style.transition = "1s"
 
 //appendChild
 mainElmt.appendChild(imgDuck);
-
 // Function
 
-window.setInterval(movingDuck, 800)
+window.setInterval(movingDuck, 1000);
 
 function movingDuck() {
     imgDuck.style.left = Math.random()*90 + "%"
     imgDuck.style.top = Math.random()*90 + "%"
 }
+var score = 0;
+scoreText.innerHTML = "Votre score est de :" + score
+function shootDuck(){
+    console.log("click shoot!");
+    score++;
+    imgDuck.src = ducksImg[ score % ducksImg.length];
+
+    scoreText.innerHTML =  "Votre score est de :" + score;
+    // window.setInterval(movingDuck, 100);
+}
+
+
+imgDuck.addEventListener('click', shootDuck );
